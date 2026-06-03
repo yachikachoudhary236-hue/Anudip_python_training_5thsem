e2e1f5232acd234067d0ce8d34641474b279efef
yachika choudhary <yachikachoudhary236@gmail.com>
2026-06-03 20:00:36 +0530

    Implement triangle area and perimeter calculations

---
+++ classwork/algorithm_3rd june /Triangle_Area_Perimeter.py
@@ -0,0 +1,8 @@
+a=int(input("enter the side a"))
+
+b=int(input("enter the side b"))
+c=int(input("enter the side c"))
+peri= (a+b+c)
+s= peri/2
+area= (s*(s-a)(s-b)(s-c))*1/2
+print("area and parameter are :" ,area, peri)
