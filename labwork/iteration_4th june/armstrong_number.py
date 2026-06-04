4727a67f4c116f6ca1ba45ab401af78c87a0cd13
yachika choudhary <yachikachoudhary236@gmail.com>
2026-06-04 14:28:30 +0530

    Update armstrong_number.py

--- labwork/iteration_4th june/armstrong_number.py
+++ labwork/iteration_4th june/armstrong_number.py
@@ -1,14 +1,24 @@
-#accept a statement from the user and check whether it is armstrong number or not.
-num = int(input("Enter a number: "))   
-sum = 0
-temp = num
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** 3
-    temp //= 10
-if num == sum:
-    print(num, "is an Armstrong number")
-else:
-    print(num, "is not an Armstrong number")
+# Program to check Armstrong number
+# Take input from user
+num = int(input("Enter a number: "))
 
-            
+# Store original number for final comparison
+original_num = num
+
+# Find number of digits
+digits = len(str(num))
+
+# Variable to store sum of digits raised to power
+sum_of_powers = 0
+
+# Loop through each digit of the number
+while num > 0:
+    digit = num % 10              # extract last digit
+    sum_of_powers += digit ** digits  # raise digit to power of total digits
+    num = num // 10               # remove last digit
+
+# Check if original number is equal to calculated sum
+if sum_of_powers == original_num:
+    print(original_num, "is an Armstrong number")
+else:
+    print(original_num, "is not an Armstrong number")
