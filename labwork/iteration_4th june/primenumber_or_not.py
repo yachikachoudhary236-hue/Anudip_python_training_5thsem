9cb10b66f83a1129235c4c0ae2370c292684cad7
yachika choudhary <yachikachoudhary236@gmail.com>
2026-06-04 14:26:09 +0530

    Update primenumber_or_not.py

--- labwork/iteration_4th june/primenumber_or_not.py
+++ labwork/iteration_4th june/primenumber_or_not.py
@@ -1,16 +1,23 @@
-#wap to analyze prime number ,accept a number from user and check whether it is prime or not.and print factor if it is not a prime number. 
-num = int(input("enter a number"))
-if(num>1):
-    for i in range(2,num):
-        if(num%i==0):
-            print(num,"is not a prime number")
-            print("Factors of", num, "are:", end=" ")
-            for j in range(1, num + 1):
-                if num % j == 0:
-                    print(j, end=" ")
-            print()
-            break
-    else:       
-        print(num,"is a prime number")
+# Program to check whether a number is prime or not
+# Take input from user
+num = int(input("Enter a number: "))
+
+# Prime numbers are greater than 1
+if num <= 1:
+    print(num, "is not a prime number")
+
 else:
-    print(num,"is not a prime number")
+    # flag variable to track prime status
+    is_prime = True
+
+    # check divisibility from 2 to num-1
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False  # number is divisible, so not prime
+            break  # no need to check further
+
+    # final result
+    if is_prime:
+        print(num, "is a prime number")
+    else:
+        print(num, "is not a prime number")
